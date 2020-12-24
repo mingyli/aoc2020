bf8192395daa0f39863aa2647e7d170a45915fe5
Ming <mingyli34@gmail.com>
2020-12-23 16:59:44 -0800

    Use recursive memoization

--- day10/day10.ml
+++ day10/day10.ml
@@ -22,57 +22,46 @@ let day10a numbers =
   let threes = count_occurrences 3 numbers in
   ones * threes
 
-(**
-  let rec _count_ways = function
-    | [] | [ _ ] -> 1
-    | [ first; second ] -> if second - first <= 3 then 1 else 0
-    | first :: (second :: (third :: (fourth :: _ as tail3) as tail2) as tail1)
-      ->
-        let a = if second - first <= 3 then _count_ways tail1 else 0 in
-        let b = if third - first <= 3 then _count_ways tail2 else 0 in
-        let c = if fourth - first <= 3 then _count_ways tail3 else 0 in
-        a + b + c
-    | first :: (second :: (third :: _ as tail2) as tail1) ->
-        let a = if second - first <= 3 then _count_ways tail1 else 0 in
-        let b = if third - first <= 3 then _count_ways tail2 else 0 in
-        a + b
-  in
-   *)
+let memoize m f =
+  let cache = Hashtbl.create m in
+  fun x -> Hashtbl.find_or_add cache x ~default:(fun () -> f x)
+
+let memoize_rec m f_norec x =
+  let fref = ref (fun _ -> assert false) in
+  let f = memoize m (fun x -> f_norec !fref x) in
+  fref := f;
+  f x
+
 let day10b numbers =
   let count_ways numbers =
-    let cache = Hashtbl.create (module Int) in
-    let rec aux index =
-      let result =
-        match Hashtbl.find cache index with
-        | Some value -> value
-        | None -> (
-            let n = Array.length numbers in
-            match index with
-            | i when i = n -> 1
-            | i when i = n - 1 -> 1
-            | i when i = n - 2 ->
-                let a = numbers.(i) in
-                let b = numbers.(i + 1) in
-                if b - a <= 3 then 1 else 0
-            | i when i = n - 3 ->
-                let a = numbers.(i) in
-                let b = numbers.(i + 1) in
-                let c = numbers.(i + 2) in
-                let x = if b - a <= 3 then aux (i + 1) else 0 in
-                let y = if c - a <= 3 then aux (i + 2) else 0 in
-                x + y
-            | i ->
-                let a = numbers.(i) in
-                let b = numbers.(i + 1) in
-                let c = numbers.(i + 2) in
-                let d = numbers.(i + 3) in
-                let x = if b - a <= 3 then aux (i + 1) else 0 in
-                let y = if c - a <= 3 then aux (i + 2) else 0 in
-                let z = if d - a <= 3 then aux (i + 3) else 0 in
-                x + y + z )
-      in
-      let (_ : unit) = Hashtbl.set cache ~key:index ~data:result in
-      result
+    let n = Array.length numbers in
+    let aux =
+      memoize_rec
+        (module Int)
+        (fun aux index ->
+          match index with
+          | i when i = n -> 1
+          | i when i = n - 1 -> 1
+          | i when i = n - 2 ->
+              let a = numbers.(i) in
+              let b = numbers.(i + 1) in
+              if b - a <= 3 then 1 else 0
+          | i when i = n - 3 ->
+              let a = numbers.(i) in
+              let b = numbers.(i + 1) in
+              let c = numbers.(i + 2) in
+              let x = if b - a <= 3 then aux (i + 1) else 0 in
+              let y = if c - a <= 3 then aux (i + 2) else 0 in
+              x + y
+          | i ->
+              let a = numbers.(i) in
+              let b = numbers.(i + 1) in
+              let c = numbers.(i + 2) in
+              let d = numbers.(i + 3) in
+              let x = if b - a <= 3 then aux (i + 1) else 0 in
+              let y = if c - a <= 3 then aux (i + 2) else 0 in
+              let z = if d - a <= 3 then aux (i + 3) else 0 in
+              x + y + z)
     in
     aux 0
   in
